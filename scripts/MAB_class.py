9757461497f7a40fefe7ef0e23d70426a5246d25
HatimRabet <hatim.mrabet@student-cs.fr>
2024-08-04 00:54:42 +0200

    adding linUCb

--- scripts/MAB_class.py
+++ scripts/MAB_class.py
@@ -184,6 +184,7 @@ class MABAgent:
         self.d = A.shape[1]
         self.theta = np.random.rand(self.d)
         self.cumulative_regret = [0]
+        self.regret = [0]
         self.V_l = np.zeros((self.d, self.d))
         self.sum_reward = np.zeros(self.d)
         self.rewardsXmovie_indices = rewardsXmovie_indices  
@@ -258,10 +259,128 @@ class MABAgent:
                 for _ in range(n_a):
                     true_reward = np.max(self.A @ self.theta)
                     reward = self.A[action_idx] @ self.theta
-                    self.cumulative_regret.append(self.cumulative_regret[-1] + (true_reward - reward))
+                    regret = (true_reward - reward)
+                    self.cumulative_regret.append(self.cumulative_regret[-1] + regret)
+                    self.regret.append(regret)
                     action_indices.append(action_idx)
                     true_rewards.append(true_reward)
             self.update(action_indices, true_rewards)
             t += N
             l += 1
+        return self.cumulative_regret, self.regret, self.theta
+
+
+
+class LinUCB_MABAgent:
+    def __init__(self, A, delta, lambda_, n_rounds, true_rewards, rewardsXmovie_indices, actionXmovie_indices):
+        self.A = A
+        self.delta = delta
+        self.n_rounds = n_rounds
+        self.true_rewards = true_rewards
+        self.rewardsXmovie_indices = rewardsXmovie_indices
+        self.actionXmovie_indices = actionXmovie_indices
+        self.lambda_ = lambda_
+        self.beta = np.sqrt(lambda_) + np.sqrt(2 * np.log(1/delta))
+        self.d = A.shape[1]
+        self.n_actions = A.shape[0]
+        self.V = lambda_*np.eye(self.d)  # Identity matrix for the initial V
+        self.b = np.zeros(self.d)  # Mean rewards vector
+        self.theta = np.zeros(self.d)
+        self.ucb_values = np.zeros(self.n_actions)
+        self.action_counts = np.zeros(self.n_actions)
+        self.cumulative_regret = [0]
+        self.regret = [0]
+        self.time = 0
+
+    def ucb_values_fn(self):
+        A_inv = np.linalg.inv(self.V)
+
+        for i in range(self.n_actions):
+            a = self.A[i]
+            ucb = a @ self.theta 
+            upper_conf = self.beta * np.sqrt(a @ A_inv @ a.T)
+            self.ucb_values[i] = ucb + upper_conf
+        
+        return 
+
+    def select_action(self):
+            
+        return np.argmax(self.ucb_values)
+    
+    def update(self, action_idx, reward):
+        a = self.A[action_idx]
+        self.V += np.outer(a, a)
+        self.b += reward * a.T
+        self.theta = np.linalg.inv(self.V) @ self.b
+        self.beta = np.sqrt(self.lambda_) + np.sqrt(2 * np.log(1/self.delta) + self.d*np.log(1 + (self.time-1)/(self.lambda_*self.d)))
+        self.action_counts[action_idx] += 1
+
+    def reward(self, action_idx):
+        movie_id = self.actionXmovie_indices[action_idx]
+        rewards_indices = np.where(self.rewardsXmovie_indices == movie_id)[0]
+        if rewards_indices.size == 0:
+            raise ValueError(f"No rewards found for movie_id {movie_id}")
+        reward_index = np.random.choice(rewards_indices)
+        return self.true_rewards[reward_index]
+
+    def run(self):
+        while self.time < self.n_rounds:
+            self.ucb_values_fn()
+            action_idx = self.select_action()
+            reward = self.reward(action_idx)
+            true_reward = np.max(self.A @ self.theta)
+            self.update(action_idx, reward)
+            regret = (true_reward - reward)
+            self.cumulative_regret.append(self.cumulative_regret[-1] + regret)
+            self.regret.append(regret)
+            self.time += 1
+        return self.cumulative_regret, self.regret, self.theta
+
+
+
+class UCB_MABAgent:
+    def __init__(self, A, delta, n_rounds, true_rewards, rewardsXmovie_indices, actionXmovie_indices):
+        self.A = A
+        self.delta = delta
+        self.n_rounds = n_rounds
+        self.true_rewards = true_rewards
+        self.d = A.shape[1]
+        self.theta = np.random.rand(self.d)
+        self.cumulative_regret = [0]
+        self.rewardsXmovie_indices = rewardsXmovie_indices  
+        self.actionXmovie_indices = actionXmovie_indices
+        self.n_actions = A.shape[0]
+        self.action_counts = np.zeros(self.n_actions)
+        self.total_reward = np.zeros(self.n_actions)
+        self.UCB_values = np.zeros(self.n_actions)
+        self.time = 0
+
+    def select_action(self):
+        # Compute UCB values
+        if self.time < self.n_actions:
+            # Select each action at least once
+            return self.time
+        self.UCB_values = self.total_reward / (self.action_counts + 1) + np.sqrt(2 * np.log(self.time + 1) / (self.action_counts + 1))
+        return np.argmax(self.UCB_values)
+
+    def update(self, action_idx, reward):
+        self.action_counts[action_idx] += 1
+        self.total_reward[action_idx] += reward
+
+    def reward(self, action_idx):
+        movie_id = self.actionXmovie_indices[action_idx]
+        rewards_indices = np.where(self.rewardsXmovie_indices == movie_id)[0]
+        if rewards_indices.size == 0:
+            raise ValueError(f"No rewards found for movie_id {movie_id}")
+        reward_index = np.random.choice(rewards_indices)
+        return self.true_rewards[reward_index]
+
+    def run(self):
+        while self.time < self.n_rounds:
+            action_idx = self.select_action()
+            reward = self.reward(action_idx)
+            self.update(action_idx, reward)
+            true_reward = np.max(self.A @ self.theta)
+            self.cumulative_regret.append(self.cumulative_regret[-1] + (true_reward - reward))
+            self.time += 1
         return self.cumulative_regret, self.theta
