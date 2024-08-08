c2ccc5c6ebec9acb8a3799c4dbf9154ba5eb5829
HatimRabet <hatim.mrabet@student-cs.fr>
2024-08-08 21:44:43 +0200

    correcting some errors in the logic of the algorithm

--- scripts/utils.py
+++ scripts/utils.py
@@ -1,12 +1,25 @@
 import numpy as np
 import math
+from scipy.linalg import svd
+import random
 
-def sample_k_movies(A ,k):
+def sample_k_movies(A ,k, m):
     """
     This function takes a matrix A (each row represents a movie)
-    and returns the matrix of just the first k movies(the ones with more ratings)
+    and returns the matrix of just the first k movies with the m forst columns (the ones with more ratings)
     """
-    return A[:k]
+    indices = random.sample([i for i in range(A.shape[0])], k)
+    return A[indices][:,:m], indices
+
+
+
+
+def matrix_rank(A, tol=None):
+    """ Compute matrix rank using SVD with a given tolerance. """
+    u, s, vh = svd(A, full_matrices=False)
+    if tol is None:
+        tol = np.max(A.shape) * np.amax(s) * np.finfo(s.dtype).eps
+    return np.sum(s > tol)
 
 
 
